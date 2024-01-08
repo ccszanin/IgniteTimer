16629f83c1b7b9bee3c68b4322c611fb64bb6c27
Carla Cristine <carlacs.cristine@gmail.com>
2024-01-08 19:33:25 -0300

    title page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,11 +50,20 @@ export function Home() {
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
  useEffect (() => {
+  let interval: number;
+
   if (activeCycle){
-    setInterval(() => {
-      setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
+       interval = setInterval(() => {
+      setAmountSecondsPassed(
+        differenceInSeconds(new Date(), activeCycle.startDate),
+        )
     }, 1000)
   }
+   
+   return () => {
+     clearInterval(interval)
+   }
+
  }, [activeCycle])
 
  function handleCreateNewCycle(data: NewCycleFormData){
@@ -68,7 +77,10 @@ export function Home() {
 
    setCycles((state) => [...state, newCycle])
    setActiveCycleId(id)
-  reset();
+   setAmountSecondsPassed(0)
+  
+  
+   reset();
  }
 
 
@@ -81,6 +93,13 @@ export function Home() {
  
  const minutes = String(minutesAmount).padStart(2, '0')
  const seconds = String(secondsAmount).padStart(2, '0')
+
+useEffect(() => {
+  if (activeCycle){
+    
+    document.title = `${minutes}:${seconds}`
+   }
+},[minutes, seconds, activeCycle])
 
  const task = watch('task')
  const isSubmitDisabled = !task
